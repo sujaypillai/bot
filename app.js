24a1baf700ea62ec56d6a74b249587c87d3b623d
Siddharth Lolap <Siddharth_Lolap@Jabil.com>
2017-05-22 13:40:31 +0800

    First push

---
+++ app.js
@@ -0,0 +1,22 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId:'faa49e3b-c439-425c-b588-88f371d05c81',
+    appPassword:'brm8UNW7TbxS9GUeu0ivCuH'
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
