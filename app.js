e5ab64caf776f10d962dcf291004b9f5612dd95c
Siddharth Lolap <Siddharth_Lolap@Jabil.com>
2017-05-25 15:52:35 +0800

    3rd push

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+var google = require('google')
+
+google.resultsPerPage = 5
+var nextCounter = 0
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -19,8 +24,25 @@ server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
 	if(session.message.text=="Hi"){
-    		session.send("You said: %s", session.message.text);
+    		session.send("%s, How can I help you?", session.message.text);
 	}else{
-		session.send("You said: %s", "I am not getting your question");
+		
+		 google(session.message.text, function (err, res){
+			if (err) console.error(err)
+ 
+			  for (var i = 0; i < res.links.length; ++i) {
+				var link = res.links[i];
+				session.send(link.title + ' - ' + link.href);
+				session.send(link.description + "\n");
+				//console.log(link.title + ' - ' + link.href)
+				//console.log(link.description + "\n")
+				}
+ 
+			  if (nextCounter < 4) {
+				nextCounter += 1
+				if (res.next) res.next()
+				}
+		})
 	}
+	
 });
